58cb6a922734cadaf1ab9bf6cebd3b3d6e91a593
Donhyun72 <try365@gmail.com>
2020-12-13 14:58:31 +0000

    Add argument

--- test.sh
+++ test.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-echo "test.sh"
-# origin test
-# local test 2
+_hello()
+{
+	echo "Usage) $0 [start|stop]"
+  return 0
+}
+
+if [ $# -eq 0 ]; then
+	_hello
+	exit 0
+fi
+
+case $1 in 
+	start)
+		echo "start ..."
+    ;;
+	stop)
+		echo "stop ..."
+		;;
+	*)
+		_hello
+		;;
+esac
